ef6544806a602f06319e6cc6b9389acfd4105cd3
Orr Shalev <shalev.orr@gmail.com>
2025-04-06 21:32:54 -0400

    cloudflare support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,18 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
