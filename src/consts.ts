9fd4cdb2091c2289538ed37e7e8f5a929a2621d7
Orr Shalev <shalev.orr@gmail.com>
2025-04-06 21:19:31 -0400

    Start

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Orr\'s J\'Blog';
+export const SITE_DESCRIPTION = 'Orr\'s J\'Blog (Journal-blog / place to write down dumb ideas)';
